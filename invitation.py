106843a8733eacfe293e6b32522b771bfc614898
adusumallioh <150758659+adusumallioh@users.noreply.github.com>
2023-11-22 13:03:18 -0500

    Update invitation.py

--- invitation.py
+++ invitation.py
@@ -1,5 +1,5 @@
 import streamlit
 
-streamlit.text("You will be Cordially Invited to the Exchanges
+streamlit.text('You will be Cordially Invited to the Exchanges
 ...as soon as you submit your Account Locator
-...using the Streamlit form LINKED HERE!");
+...using the Streamlit form LINKED HERE!');
